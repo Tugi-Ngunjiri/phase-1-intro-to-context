648057491e3c7afa7b411507c0f5b2be6b6e476a
Your Name <you@example.com>
2022-12-08 22:53:04 +0300

    finished my assigment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function createEmployeeRecord(){
+const FirstName = "Geoffrey";
+console.log("Geoffery");
+}
+function createFamilyName(){
+const FamilyName = "Rodger's";
+console.log("Rodger's");
+}
+
+
+function  createGeneralManager(){
+const Title = "GeneralManager";
+console.log("GeneralManager");
+}
+
+
+function createPay(){
+const Pay = "50,000";
+console.log("50,000");
+}
+
+
+function createTimeInEvent(){
+const TimeinEvents = "2.00pm";
+console.log("2.00pm");
+}
+
+
+function createTimeOutEvent(){
+const TimeOutEvents = "6.00pm"
+console.log("6.00pm")
+}
+
