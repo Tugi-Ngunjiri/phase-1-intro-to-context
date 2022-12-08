f4a70461a7fcc7e7b5d3cee9e544fb77c3696c76
Your Name <you@example.com>
2022-12-08 23:43:23 +0300

    finished with my assigment

--- index.js
+++ index.js
@@ -16,19 +16,46 @@ console.log("GeneralManager");
 
 
 function createPay(){
-const Pay = "50,000";
-console.log("50,000");
+const Pay = "50,000 on 15 th May 2022";
+console.log("50,000 on 15 th May 2022");
 }
 
 
 function createTimeInEvent(){
-const TimeinEvents = "2.00pm";
-console.log("2.00pm");
+const TimeinEvents = "Beginning of work Day at 1.00pm";
+console.log("Beginning of Work Day at 1.00pm to 6.00pm");
 }
 
 
 function createTimeOutEvent(){
-const TimeOutEvents = "6.00pm"
-console.log("6.00pm")
+const TimeOutEvents = "End of Day; at 6.00pm"
+console.log("End of Working Day at 6.00pm")
 }
 
+function DebtRecord(){
+const DebtToPay  = "Debt of 20.000 to pay by 15 th April 2022"
+console.log("Debt of 20,00 to pay before 15 th April 2022")
+
+}
+ function  createWorkHours(){
+const HoursWorked = "Worked for 5hours"
+console.log("5hoursworked")
+
+ }
+
+ function createWagesEarned(){
+const WagesEarned = " Wages between 50,000k to 100,000k"
+console.log("Wages between 50,000k and 100,000k ")
+
+ }
+
+ function createAllWagesForGeoffery(){
+const allWages = "Up to 100,000"
+console.log("up to 100,000k")
+
+ }
+  function CreatePayRoll(){
+const PayRoll = "100,000k"
+console.log("100,00k")
+
+  }
